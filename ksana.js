1703d3668c7c850ca081f20fee8706e75ea5a082
ben29016117 <a0936727522@gmail.com>
2015-03-10 15:19:59 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "my-app",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/ben29016117/my-app/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145175,
+  277980,
+  388,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:12:30.112Z",
+  "2015-03-10T07:12:30.111Z",
+  "2015-03-10T07:12:30.169Z",
+  "2015-03-10T07:13:24.604Z",
+  "2015-03-10T07:12:30.133Z",
+  "2015-03-10T07:12:30.135Z",
+  "2015-03-10T07:12:30.085Z",
+  "2015-03-10T07:12:30.075Z",
+  "2015-03-10T07:13:06.854Z"
+ ],
+ "date": "2015-03-10T07:13:25.574Z"
+})
